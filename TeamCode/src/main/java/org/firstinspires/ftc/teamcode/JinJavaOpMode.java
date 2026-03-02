e8b52ccf0b8ae3ed527f4cd3af3c0f7da9e8f680
Jin Zhou <ferryzhou@gmail.com>
2026-03-01 18:54:13 -0800

    Replace Gyroscope with IMU to fix 'unable to find a hardware device with name "imu" and type Gyroscope' error. Also removed  DigitalChannel and DistanceSensor to simplify the initial code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JinJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JinJavaOpMode.java
@@ -3,25 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class JinJavaOpMode extends LinearOpMode {
-    private Gyroscope imu;
+    private IMU imu;
     private DcMotor motorTest;
-    private DigitalChannel digitalTouch;
-    private DistanceSensor sensorColorRange;
     private Servo servoTest;
 
     @Override
     public void runOpMode() {
-        imu = hardwareMap.get(Gyroscope.class, "imu");
+        imu = hardwareMap.get(IMU.class, "imu");
         motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
         servoTest = hardwareMap.get(Servo.class, "servoTest");
 
         telemetry.addData("Status", "Initialized");
